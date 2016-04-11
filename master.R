4d1ee38fcbde231c76628e9b4b8adb8ed198863d
Eric Taylor <ericgtaylor@gmail.com>
2016-04-11 12:01:55 -0500

    ready for rf

--- master.R
+++ master.R
@@ -69,22 +69,45 @@ tdat=prep.for.tree(tdat)
 tdat.prepped=tdat # took a while to do this so save
 # first have to remove categorical variables with more than 53 categories
 cat.vars=sapply(tdat,class)
-cat.vars.nlevels=sapply(tdat,function(x) )
+cat.vars.nlevels=sapply(tdat[,cat.vars=="factor"],function(x) length(unique(x)))
+sort(cat.vars.nlevels)
+# rfa_x (60-122 levels): remove the first bite, recency, to reduce the number of unique codes
+rfas=paste0("rfa_",2:24)
+tdat[,rfas]=substr(as.matrix(tdat[,rfas]),2,10)
+# tcode (55 levels): remove a random 2 tcode levels with frequency 1
+tcodes=table(tdat$tcode)
+# do this once and save them so you can remove from vdat as well
+# tcodes.rm=sample(rownames(tcodes[tcodes==1]),3)
+tcodes.rm=c("tc76","tc24002","tc27")
+tdat=filter(tdat,!(tcode%in%tcodes.rm))
+tdat$tcode=as.factor(as.character(tdat$tcode))
+# state (57 levels): would re-code by region but to move quickly, remove 4 least frequent
+states=table(tdat$state)
+tdat=filter(tdat,!(state%in%names(sort(states))[1:5]))
+tdat$state=as.factor(as.character(tdat$state))
+# osource (895 levels): just remove it... no time to worry about this
+tdat=select(tdat,-osource)
+# zip (~20k levels): just remove it since you already have state and there's way too many
+tdat=select(tdat,-zip)
 
 tr.prop=.75
 train.indxs=sample(1:nrow(tdat),round(nrow(tdat)*tr.prop))
 rm.vars=-match(c("controln","target_b"),colnames(tdat))
 tdat.train=tdat[train.indxs,rm.vars]
 tdat.test=tdat[-train.indxs,rm.vars]
+
+cat.vars=sapply(tdat.train,class)
+cat.vars.nlevels=sapply(tdat.train[,cat.vars=="factor"],function(x) length(unique(x)))
+sort(cat.vars.nlevels)
+
 # do the random forest
 # learned a while back not to call the formula (see tip on the list below)
 # http://stats.stackexchange.com/questions/37370/random-forest-computing-time-in-r
-tdat.rf=randomForest(tdat.train[,colnames(tdat.train)!="target_d"],
+tdat.train.IVs=tdat.train[,!(colnames(tdat.train)%in%c("target_d"))]
+tdat.train.IVs=prep.for.tree(tdat.train.IVs)
+tdat.rf=randomForest(tdat.train.IVs,
                      tdat.train$target_d,importance=T,ntree=10,do.trace=T)
 tdat.rf.pred=predict(tdat.rf,select(tdat.test,-churned),type="prob")
 tdat.rf.pred.range=predict(tdat.rf,select(tdat.test,-churned),type="response")
 tdat.rf.pred.YN=data.frame(yes=1-tdat.rf.pred[,"no_churn"],
                            no=tdat.rf.pred[,"no_churn"])
-
-
-# don't forget to exclue the DVs from the IV feature set
